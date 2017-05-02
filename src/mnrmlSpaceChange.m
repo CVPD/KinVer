c25df878575f8c19462b2bbc66a0560389af3162
oserradilla <oscserradilla@gmail.com>
2017-05-02 17:16:26 +0200

    Added PCA to MNRML

--- src/mnrmlSpaceChange.m
+++ src/mnrmlSpaceChange.m
@@ -1,5 +1,5 @@
 % function mnrmlSpaceChange(inputFile,outputFile)
-% 
+%
 % Transforms the input data arranged to perform classification to the MNRML
 % created space.
 % Input: inputFile; data ready to perform classification per fold
@@ -26,7 +26,7 @@ nfold = size(Xtra,2);
 K = size(Xtra{1},1);
 
 for c = 1:nfold
-
+    
     % Display number of fold processing
     txt = strcat('fold number', string(c));
     disp(txt)
@@ -34,12 +34,28 @@ for c = 1:nfold
     
     %% select dataset rows given indexes
     for p = 1:K
-        Xa = Xtra{c}{p};
-        Xb = Xtrb{c}{p};
-        tr_Xa_pos{p} = Xa(tr_matches{c}, :); % positive training data
-        tr_Xb_pos{p} = Xb(tr_matches{c}, :); % positive training data
-        clear Xa Xb;
+        tr_Xa_pos{p} = Xtra{c}{p}(tr_matches{c}, :); % positive training data
+        tr_Xb_pos{p} = Xtrb{c}{p}(tr_matches{c}, :); % positive training data
     end
+    
+    %% do PCA  on training data (comment this for loop to remove PCA)
+    for p = 1:K
+        % Calculate eigen vectors and values
+        [eigvec, eigval, ~, sampleMean] = PCA([Xtra{c}{p}; Xtrb{c}{p}]);
+        Wdims = size(eigvec, 2); % Calculate dimension of matrix
+        
+        % Apply PCA to all data
+        Xtra{c}{p} = applyPCA(Xtra{c}{p},eigvec,sampleMean,Wdims);
+        Xtrb{c}{p} = applyPCA(Xtrb{c}{p},eigvec,sampleMean,Wdims);
+                
+        Xtsa{c}{p} = applyPCA(Xtsa{c}{p},eigvec,sampleMean,Wdims);
+        Xtsb{c}{p} = applyPCA(Xtsb{c}{p},eigvec,sampleMean,Wdims);
+        
+        tr_Xa_pos{p} = applyPCA(tr_Xa_pos{p},eigvec,sampleMean,Wdims);
+        tr_Xb_pos{p} = applyPCA(tr_Xb_pos{p},eigvec,sampleMean,Wdims);
+        
+    end
+    
     %% MNRML
     [W, beta{c}] = mnrml_train(tr_Xa_pos, tr_Xb_pos, knn, Wdims, T);
     
@@ -53,4 +69,17 @@ for c = 1:nfold
 end
 
 save(outputFile, 'Xtra', 'Xtrb', 'Xtsa', 'Xtsb', 'tr_matches', 'ts_matches', 'beta');
+end
+
+% Input: X; Data to perform PCA
+% Output: X; PCA performed in the input data
+function X = applyPCA(X,eigvec,sampleMean,Wdims)
+
+X = (bsxfun(@minus, X, sampleMean) * eigvec(:, 1:Wdims));
+
+N = size(X, 1);
+for i = 1:N
+    X(i, :) = X(i, :) / norm(X(i, :));
+end
+
 end
