7aff249fb48a96b06f10ebab4a811a567c7fe8e3
oserradilla <oscserradilla@gmail.com>
2017-05-15 09:46:37 +0200

    MNRML parameter setting
    The parameters of MNRML are fixed from the values of the original paper and then changing one at a time to set the best value for classification and expecting that improving each variable at a time gets a good setting of the parameters together.

--- src/main.m
+++ src/main.m
@@ -32,20 +32,36 @@ end
 %%% End of variables initialization %%%
 
 %% Classification
-parfor pairIdx = 1:size(featuresFileNames,1)
-    [accuracy{pairIdx},accuracyMNRML{pairIdx},accuracyNRML{pairIdx}] = ...
-        performClassification(imagePairsDirs(pairIdx,:), convnetDir, ...
-            featuresFileNames(pairIdx,:), metadataPairs(pairIdx,:), ...
-            pairIdStrs(pairIdx,:), vggMatFileNames(pairIdx,:), ...
-            imagenetMatFileNames(pairIdx,:), ...
-            10, 10, 40);
+numIt = 1;
+rangeIt = 5:5:100;
+for Wdims = rangeIt
+    T = 4;
+    knn = 6;
+    %Wdims = 30;
+    parfor pairIdx = 1:size(featuresFileNames,1)
+
+        [accuracy(pairIdx),accuracyMNRML(pairIdx),accuracyNRML(pairIdx)] = ...
+            performClassification(imagePairsDirs(pairIdx,:), convnetDir, ...
+                featuresFileNames(pairIdx,:), metadataPairs(pairIdx,:), ...
+                pairIdStrs(pairIdx,:), vggMatFileNames(pairIdx,:), ...
+                imagenetMatFileNames(pairIdx,:), ...
+                T, knn, Wdims);
+    end
+    meanAccuracy(numIt) = mean(accuracyMNRML);
+    numIt = numIt + 1;
 end
-%%% End of feature extraction %%%
+meanAccuracy = meanAccuracy*100;
+p = plot(rangeIt,meanAccuracy);
+xlabel('Feature dimension');
+ylabel('Verification accuracy (%)');
+set(p,'Marker','o','MarkerEdgeColor','b','MarkerFaceColor','b');
+%%% End of classification %%%
 
 function [accuracy, accuracyMNRML, accuracyNRML] = performClassification(...
     imagePairsDir, convnetDir, featuresFileName, metadataPair, ...
     pairIdStr, vggMatFileName, imagenetMatFileName, T, knn, Wdims)
 K = 2;
+accuracy = 0; accuracyMNRML = 0; accuracyNRML = 0;
 %   ;calculateSaveFeatures(imagePairsDir,convnetDir,featuresFileName);
 %    cosineROCPlot(featuresFileName,metadataPair,pairIdStr);
 arrangeDataInPairs(featuresFileName,metadataPair,...
@@ -58,14 +74,14 @@ load(imagenetMatFileName);
 fea{2} = ux;
 
 % Classification on original features
-accuracy = pairSVMClassification(fea, idxa, idxb, fold, matches, K, 1/K);
+%accuracy = pairSVMClassification(fea, idxa, idxb, fold, matches, K, 1/K);
 
 % Classification on MNRML
 [projFea, ~, projBeta] = mnrmlProjection(fea, idxa, idxb, fold, matches, K, T, knn, Wdims);
 accuracyMNRML = pairSVMClassification(projFea, idxa, idxb, fold, matches, K, projBeta);
 
 % Classification on NRML
-projFeaNRML = nrmlProjection(fea, idxa, idxb, fold, matches, K, T, knn, Wdims);
-accuracyNRML = pairSVMClassification(projFeaNRML, idxa, idxb, fold, matches, K, 1/K);
+%projFeaNRML = nrmlProjection(fea, idxa, idxb, fold, matches, K, T, knn, Wdims);
+%accuracyNRML = pairSVMClassification(projFeaNRML, idxa, idxb, fold, matches, K, 1/K);
 
 end
