1a277214a2521ce61ab6639936435484ca0e0fd3
oserradilla <oscserradilla@gmail.com>
2017-06-06 15:48:40 +0200

    LBP and HOG added

--- src/arrangeDataInPairs.m
+++ src/arrangeDataInPairs.m
@@ -1,4 +1,5 @@
-function arrangeDataInPairs(dataFile,metadataDir,VGGFileName,imagenetFileName)
+function arrangeDataInPairs(dataFile,metadataDir,vggFaceFileName,...
+    vggFFileName, LBPFileName, HOGFileName)
 
 load(dataFile);
 load(metadataDir);
@@ -8,37 +9,39 @@ for idx = 1:length(data)
     names{idx} = data{idx}.name;
 end
 
-% vgg-face features to matrix
+%% vgg-face features to matrix
 for idx = 1:length(data)
     ux(idx,:) = data{idx}.vggFaceFeat;
 end
+saveDataStructured(ux,pairs,names, vggFaceFileName);
+clear ux;
 
-% Pairs to idxs
-for idx = 1:length(pairs)
-    % Find vectors of the pair
-    vec1Idx = find(ismember(names,pairs(idx,3)));
-    vec2Idx = find(ismember(names,pairs(idx,4)));
-    if isempty(vec1Idx) || isempty(vec2Idx)
-        error('One of the pair images is not found in the processed data');
-    end
-    idxa(idx,:) = vec1Idx;
-    idxb(idx,:) = vec2Idx;
-    numFoldCell = pairs(idx,1); % number of fold to perform CV
-    fold(idx,:) = numFoldCell{1};
-    realClassCell = pairs(idx,2); % real class
-    matches(idx,:) = realClassCell{1};
+%% vgg-f features to matrix
+for idx = 1:length(data)
+    ux(idx,:) = data{idx}.vggFFeat;
 end
-
-matches = logical(matches); % turn it into boolean
-
-save(VGGFileName, 'ux', 'matches', 'idxa', 'idxb', 'fold');
+saveDataStructured(ux,pairs,names,vggFFileName);
+clear ux;
 
 clear ux matches idxa idxb fold;
 
-% vgg-f features to matrix
+%% LBP features to matrix
 for idx = 1:length(data)
-        ux(idx,:) = data{idx}.vggFFeat;
+    ux(idx,:) = data{idx}.LBPFeat;
 end
+saveDataStructured(ux,pairs,names, LBPFileName);
+clear ux;
+
+%% HOG features to matrix
+for idx = 1:length(data)
+    ux(idx,:) = data{idx}.HOGFeat;
+end
+saveDataStructured(ux,pairs,names, HOGFileName);
+clear ux;
+
+end
+
+function saveDataStructured(ux,pairs,names,fileName)
 
 % Pairs to idxs
 for idx = 1:length(pairs)
@@ -58,7 +61,7 @@ end
 
 matches = logical(matches); % turn it into boolean
 
-save(imagenetFileName, 'ux', 'matches', 'idxa', 'idxb', 'fold');
+save(fileName, 'ux', 'matches', 'idxa', 'idxb', 'fold');
 
 clear ux matches idxa idxb fold;
 
