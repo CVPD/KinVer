c5595e0163dd0a6897402ba3c078f138a772f076
Ignacio Arganda-Carreras <ignacio.arganda@gmail.com>
2018-01-31 10:57:03 +0100

    Fix bug on matconvnet folder name and compile it.

--- src/setupCNNs.m
+++ src/setupCNNs.m
@@ -6,6 +6,15 @@ oldDir = cd('..');
 untar('http://www.vlfeat.org/matconvnet/download/matconvnet-1.0-beta23.tar.gz',...
     'matconvnet') ;
 cd matconvnet
+% Move folder contents to upper folder (matconvnet)
+movefile matconvnet-1.0-beta23/* .
+% Remove zip folder
+rmdir matconvnet-1.0-beta23 s
+
+% Compile MatConvNet
+addpath matlab
+vl_compilenn('enableGpu', true)
+
 % Download a pre-trained VGG-Face CNN from the web (needed once).
 urlwrite(...
  'http://www.vlfeat.org/matconvnet/models/vgg-face.mat', ...
